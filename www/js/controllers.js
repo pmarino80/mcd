56feebdae6f0c4c7c06f90db7cb9633ca16c4488
Paul Marino <paul.marino@waterfield.com>
2014-05-23 16:36:53 -0500

    Added app files

---
+++ www/js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('starter.controllers', [])
+
+.controller('SignInCtrl', function($scope, $state) {
+  
+  $scope.signIn = function(user) {
+    console.log('Sign-In', user);
+    $state.go('app.profile');
+  };
+  
+})
+
+.controller('AppCtrl', function($scope) {
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
